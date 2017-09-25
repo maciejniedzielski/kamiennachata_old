fdc365769c53680e90d85bbcb02d45d83696f0cd
Introoz <niedzielamac@gmail.com>
2017-09-25 20:51:03 +0200

    Adding sticky navigation bar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
+/*jshint esversion: 6 */
   $(document).ready(function(){
 
+    //mixitup  configuration
     var mixer = mixitup('.gallery-container',
     {
         "animation": {
@@ -17,7 +19,7 @@
       $('.gallery-photos').addClass('gallery-photos-more');
     });
 
-
+    //owl carousel configuration
       $('.owl-carousel').owlCarousel({
           loop:true,
           margin:10,
@@ -33,74 +35,93 @@
           }
       });
 
-$('a[href*="#"]:not([href="#"])').on('click', function(event) {
 
-  var target = $(this.getAttribute('href'));
+  //light slide
+  $('a[href*="#"]:not([href="#"])').on('click', function(event) {
 
-  if( target.length ) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-          scrollTop: target.offset().top
-      }, 600);
-  }
-});
+    var target = $(this.getAttribute('href'));
 
-function waypoint_photo(){
-  $('#photo-waypoint').waypoint(function(direction) {
-    $('#photo-waypoint').addClass('animated zoomIn');
-  }, {
-    offset: '100%'
-  });}
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 600);
+    }
+  });
 
-function waypoint_info(){
-  $('#info-waypoint').waypoint(function(direction) {
-    $('#info-waypoint').addClass('animated slideInLeft');
-  }, {
-    offset: '100%'
-  });}
+  //sticky nav
+  $(window).on("scroll",  function (evt){
+    evt.preventDefault();
+  });
 
-function waypoint_conveniances(){
-  $('#conveniances-waypoint').waypoint(function(direction) {
-    $('#conveniances-waypoint').addClass('animated slideInRight');
-  }, {
-    offset: '100%'
-  });}
+  let navPosition = $(".main-nav").offset().top;
 
-function waypoint_gallery(){
-  $('#gallery-waypoint').waypoint(function(direction) {
-  	$('#gallery-waypoint').addClass('animated zoomIn');
-  }, {
-  	offset: '100%'
-  });}
+  $(window).on("scroll", function(){
+    if($(window).scrollTop()-75 > navPosition) {
+      $(".main-nav").addClass("main-nav-sticky");
+    } else {
+      $(".main-nav").removeClass("main-nav-sticky");
+    }
+  });
 
-function waypoint_karnity(){
-	$('#karnity-waypoint').waypoint(function(direction) {
-		$('#karnity-waypoint').addClass('animated fadeInLeft');
-	}, {
-		offset: '100%'
-	});}
 
-function waypoint_murawki(){
-	$('#murawki-waypoint').waypoint(function(direction) {
-  	$('#murawki-waypoint').addClass('animated fadeInRight');
-  }, {
-		offset: '100%'
-	});}
+  // waypoints configuration
+  function waypoint_photo(){
+    $('#photo-waypoint').waypoint(function(direction) {
+      $('#photo-waypoint').addClass('animated zoomIn');
+    }, {
+      offset: '100%'
+    });}
 
-function waypoint_contact(){
-	$('.contact-waypoint').waypoint(function(direction) {
-  	$('.contact-waypoint').addClass('animated slideInLeft');
-  }, {
-  	offset: '100%'
-  });}
+  function waypoint_info(){
+    $('#info-waypoint').waypoint(function(direction) {
+      $('#info-waypoint').addClass('animated slideInLeft');
+    }, {
+      offset: '100%'
+    });}
 
-waypoint_info();
-waypoint_photo();
-waypoint_conveniances();
-waypoint_gallery();
-waypoint_karnity();
-waypoint_murawki();
-waypoint_contact();
+  function waypoint_conveniances(){
+    $('#conveniances-waypoint').waypoint(function(direction) {
+      $('#conveniances-waypoint').addClass('animated slideInRight');
+    }, {
+      offset: '100%'
+    });}
+
+  function waypoint_gallery(){
+    $('#gallery-waypoint').waypoint(function(direction) {
+    	$('#gallery-waypoint').addClass('animated zoomIn');
+    }, {
+    	offset: '100%'
+    });}
+
+  function waypoint_karnity(){
+  	$('#karnity-waypoint').waypoint(function(direction) {
+  		$('#karnity-waypoint').addClass('animated fadeInLeft');
+  	}, {
+  		offset: '100%'
+  	});}
+
+  function waypoint_murawki(){
+  	$('#murawki-waypoint').waypoint(function(direction) {
+    	$('#murawki-waypoint').addClass('animated fadeInRight');
+    }, {
+  		offset: '100%'
+  	});}
+
+  function waypoint_contact(){
+  	$('.contact-waypoint').waypoint(function(direction) {
+    	$('.contact-waypoint').addClass('animated slideInLeft');
+    }, {
+    	offset: '100%'
+    });}
+
+  waypoint_info();
+  waypoint_photo();
+  waypoint_conveniances();
+  waypoint_gallery();
+  waypoint_karnity();
+  waypoint_murawki();
+  waypoint_contact();
 
 
 });
